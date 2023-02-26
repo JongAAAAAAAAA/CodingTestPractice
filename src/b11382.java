39fcd2eca58457894f46cdf32fb8db368f051391
jay <wlguswhd0809@naver.com>
2023-02-26 20:24:49 +0900

    꼬마 정민

---
+++ src/b11382.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class b11382 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Long A = scanner.nextLong();
+        Long B = scanner.nextLong();
+        Long C = scanner.nextLong();
+
+        System.out.println(A+B+C);
+    }
+}
