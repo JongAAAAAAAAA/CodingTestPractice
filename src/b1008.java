70d4d8d10d440f5522069c07863444173b5dad0a
jay <wlguswhd0809@naver.com>
2023-02-26 19:37:17 +0900

    나눗셈

---
+++ src/b1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class b1008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+
+        System.out.println(A/B);
+    }
+}
