04b59b0ac7eee5cdeb7a851fef3ba6073282a322
jay <wlguswhd0809@naver.com>
2023-02-27 19:13:50 +0900

    사분면 고르기

---
+++ src/b14681.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class b14681 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if (A > 0 && B > 0)
+            System.out.println("1");
+        else if (A < 0 && B > 0)
+            System.out.println("2");
+        else if (A < 0 && B < 0)
+            System.out.println("3");
+        else if (A > 0 && B < 0)
+            System.out.println("4");
+
+    }
+}
