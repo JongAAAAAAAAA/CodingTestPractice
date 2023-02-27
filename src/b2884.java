48a5322657aa7ccbd90b3962755fedd4ce4d1e73
jay <wlguswhd0809@naver.com>
2023-02-27 19:29:26 +0900

    알람 시계

---
+++ src/b2884.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class b2884 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int H = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        if (M < 45) {
+            int last = 45 - M;
+            M = 60 - last;
+
+            if (H == 0)
+                H = 23;
+            else
+                H -= 1;
+
+        } else
+            M -= 45;
+
+        System.out.println(H + " " + M);
+    }
+}
