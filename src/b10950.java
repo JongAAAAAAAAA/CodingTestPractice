9834001ccc8f6f651ad69b77b271b48fd9cb6002
jay <wlguswhd0809@naver.com>
2023-03-05 17:37:58 +0900

    A+B - 3

---
+++ src/b10950.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class b10950 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println(a+b);
+        }
+    }
+}
