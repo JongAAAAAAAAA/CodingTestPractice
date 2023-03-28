2233ac6df72ee97c4aaaa775c76e689d3be3385b
jay <wlguswhd0809@naver.com>
2023-03-28 17:50:37 +0900

    보물

---
+++ src/b1026.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class b1026 {
+    public static void S(int[] A, int[] B) {
+        int total = 0;
+
+        int max = 0;
+
+        Arrays.sort(A);
+
+        int[] maxArr = new int[A.length];
+
+        for (int i = 0; i < A.length; i++) {
+
+            for (int j = 0; j < A.length; j++) {
+                if (B[j] > max) {
+                    max = B[j];
+                    maxArr[i] = max;
+                }
+            }
+            for (int k = 0; k < A.length; k++) {
+                if (B[k] == max) {
+                    B[k] -= max;
+                    max = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            total += A[i] * maxArr[i];
+        }
+
+        System.out.println(total);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        for (int i = 0; i < A.length; i++) {
+            A[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            B[i] = scanner.nextInt();
+        }
+
+        S(A, B);
+    }
+}
+
