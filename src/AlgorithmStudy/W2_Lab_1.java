6ae1664df1224e154afb173514771de86070322f
jay <wlguswhd0809@naver.com>
2023-04-03 14:07:56 +0900

    W2_Lab_1 Update

--- src/AlgorithmStudy/W2_Lab_1.java
+++ src/AlgorithmStudy/W2_Lab_1.java
@@ -4,30 +4,29 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class W2_Lab_1 {
-    static void binarySearch(int[] arr, int M) {
+    static void binarySearch(long[] arr, int M) {
         int lines = M; // 만들어야하는 줄 개수
-        int[] bsArr = arr; // 입력한 정렬된 배열
+        long[] bsArr = arr; // 입력한 정렬된 배열
 
-        int low = 1; // 최소 줄 길이
-        int high = bsArr[arr.length - 1]; // 최대 줄 길이
+        long low = 1; // 최소 줄 길이
+        long high = bsArr[arr.length - 1]; // 최대 줄 길이
 
         while (high >= low) {
             int cnt = 0;
 
-            int mid = (high + low) / 2;
+            long mid = (high + low) / 2;
 
             for (int i = 0; i < arr.length; i++) {
                 cnt += arr[i] / mid;
             }
 
             if (lines > cnt) {
-                high = mid;
+                high = mid -1 ;
             } else
                 low = mid + 1;
-            //low = mid = 1 //헷갈린다. 로직 다시봐바야할듯?
         }
 
-        System.out.println("low = " + low);
+        System.out.println((high + low) / 2);
 
     }
 
@@ -37,7 +36,7 @@ public class W2_Lab_1 {
         int N = scanner.nextInt(); // 원래 가지고있는 줄 개수
         int M = scanner.nextInt(); // 만드려는 줄 개수
 
-        int arr[] = new int[N];
+        long arr[] = new long[N];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scanner.nextInt();
